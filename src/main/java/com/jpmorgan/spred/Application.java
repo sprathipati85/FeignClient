fc3a7d6f3c877985897598f369ede6730158e376
sprathipati <sprathipati85@gmail.com>
2017-02-07 22:09:30 +0000

    first commit

---
+++ src/main/java/com/jpmorgan/spred/Application.java
@@ -0,0 +1,14 @@
+package com.jpmorgan.spred;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("product Application");
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
